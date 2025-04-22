91521af30f021bc2052fa1cbacec582859ac3009
juri117 <juri.bieler@gmail.com>
2025-04-22 10:59:04 +0200

    restructe project

--- usb_list.py
+++
@@ -1,9 +0,0 @@
-import usb.core
-import usb.util
-
-# Find all connected USB devices
-devices = usb.core.find(find_all=True)
-
-# Print information about each device
-for device in devices:
-    print(f"Device: {device.idVendor}:{device.idProduct}")
